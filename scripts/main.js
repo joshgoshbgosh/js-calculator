db0ac8f6caf06b84ce6c32c9c51bf1a62e58d394
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-09-07 22:11:19 -0400

    calculater display

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,24 @@
 
-//(function) {
-  //"use strict"
-//   console.log(document.getElementsByClassName("number"))
-// document.getElementsByClassName("number").addEventListener("click", pushNumber);
 var calculation = []
+
+const display = document.querySelector(".calculator-screen");
+const operator = document.querySelectorAll(".operator");
+const number1 = document.querySelectorAll(".number");
+const long0 = document.querySelectorAll("number button-lg");
+const myAnswer = document.querySelectorAll("equal-sign");
+
 function pushNumber(digit){
   console.log("digit",digit)
-calculation.push(digit)
-
+  calculation.push(digit)
+  display.setAttribute("value", calculation.toString());
+  console.log("display",display)
 }
 
 
-//}
+
 function pushOperator(opt){
   console.log("more",opt)
-calculation.push(opt)
+  calculation.push(opt)
 
 }
 
@@ -22,4 +26,11 @@ calculation.push(opt)
 
 function calculate(answer){
   console.log("give me the",answer,calculation)
+}
+
+
+
+function reset(){
+  calculation =[]
+
 }
